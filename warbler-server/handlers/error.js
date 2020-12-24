47074c534e2243aa866cc9cbab60d66ddd39102a
shubhammehra4 <shubhammehra4@gmail.com>
2020-12-24 22:13:14 +0530

    done

--- warbler-server/handlers/error.js
+++
@@ -1,9 +0,0 @@
-function errorHandler(error, _req, res, _next) {
-    return res.status(error.status || 500).json({
-        error: {
-            message: error.message || "Oops! Something went wrong."
-        }
-    })
-}
-
-module.exports = errorHandler;
