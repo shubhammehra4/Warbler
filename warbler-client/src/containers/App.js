e771ab9fd91d23650be3e21e63b5c3f3bec1b1f0
shubhammehra4 <shubhammehra4@gmail.com>
2020-12-24 22:11:49 +0530

    ignore env variables

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from "react-redux";
+import { configureStore } from "../store/index";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from './NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const store = configureStore();
 
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className="onboarding">
+        <NavBar />
+      </div>
+    </Router>
+  </Provider>
+)
 export default App;
